f06d1184dd2eac06d49dddd07e75c15095240b20
Edward Li <edward@barnabasrobotics.com>
2019-07-12 16:11:40 -0700

    Add files via upload

---
+++ waving_robot.ino
@@ -0,0 +1,36 @@
+#include <Servo.h>
+
+Servo hand;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(12,INPUT);
+  pinMode(9,OUTPUT);
+  pinMode(6,OUTPUT);
+  hand.attach(9);
+  hand.write(100);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(digitalRead(12));
+
+  if(digitalRead(12)) {
+      digitalWrite(6,HIGH);
+      for (int i=0;i<3;i++) {
+        hand.write(80);
+        delay(100);
+        hand.write(130);
+        delay(100);
+      }
+      hand.write(100);
+      // wait for the hand to settle down before polling again
+      // without the delay, you may get a false trigger
+      delay(1000);
+      digitalWrite(6,LOW);
+      
+  }
+  
+
+}
